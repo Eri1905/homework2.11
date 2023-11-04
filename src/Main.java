5a59d187897e6a31ff1b3a832d168866abe293ad
Eri1905 <ery.mtl1905@gmail.com>
2023-11-04 22:02:06 +0200

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("The first branch ever.");
-
-        System.out.println("The second conflict branch.");
-
+        System.out.println("The seconf conflict commit");
     }
 }
