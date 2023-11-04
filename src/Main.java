48e7d533c223f834aab0fb45378bc0051cba28b6
Eri1905 <ery.mtl1905@gmail.com>
2023-11-04 21:59:10 +0200

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("The first brunch ever.");
+        System.out.println("The first branch ever.");
+
+        System.out.println("The second conflict branch.");
 
     }
 }
